04f10105142f975eef19bb3e778f6e7f6efb3f35
guxuhong <1965963961@qq.com>
2016-01-12 15:45:59 +0800

    Create 188. Best Time to Buy and Sell Stock IV.py

---
+++ 188. Best Time to Buy and Sell Stock IV.py
@@ -0,0 +1,8 @@
+"""
+Say you have an array for which the ith element is the price of a given stock on day i.
+Design an algorithm to find the maximum profit. You may complete at most k transactions.
+Note:
+ You may not engage in multiple transactions at the same time (ie, you must sell the stock before you buy again).
+"""
+
+#http://liangjiabin.com/blog/2015/04/leetcode-best-time-to-buy-and-sell-stock.html
