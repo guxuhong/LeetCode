9e9c4dcd7be20e6e5fa8c0edc4214c6fcdf4f6a2
guxuhong <1965963961@qq.com>
2016-02-16 17:08:39 +0800

    Create 155. Min Stack.py

---
+++ 155. Min Stack.py
@@ -0,0 +1,45 @@
+"""
+Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+
+push(x) -- Push element x onto stack.
+pop() -- Removes the element on top of the stack.
+top() -- Get the top element.
+getMin() -- Retrieve the minimum element in the stack.
+"""
+
+class MinStack(object):
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.stack = []
+        self.minstack = []
+
+    def push(self, x):
+        """
+        :type x: int
+        :rtype: nothing
+        """
+        self.stack.append(x)
+        if self.minstack == [] or self.minstack[-1] >= x:
+            self.minstack.append(x)
+
+    def pop(self):
+        """
+        :rtype: nothing
+        """
+        if self.top() == self.getMin():
+            self.minstack.pop()
+        return self.stack.pop()
+
+    def top(self):
+        """
+        :rtype: int
+        """
+        return self.stack[-1]
+
+    def getMin(self):
+        """
+        :rtype: int
+        """
+        return self.minstack[-1]
